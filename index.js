435949bac64abd5f9cbbc64082e0473421f5b6d0
Bob-Bobinson <95943431+KuromeSama6@users.noreply.github.com>
2024-03-12 21:16:36 -0400

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 2221;
+const port = 9399;
 
 // Serve static files from the 'static' directory
 app.use(express.static(path.join(__dirname, 'static')));
